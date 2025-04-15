a8049b7b5345a65ed4c97e9b27e760b62e9fb7ab
anukultyagi <anukultyagi321@gmail.com>
2025-04-15 22:42:26 +0530

    navbar added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+
+
+function App() {
+
+  return (
+    <main className="min-h-screen dark:bg-zinc-900 bg-[#ededed] dark:text-zinc-300 text-zinc-800">
+      <Navbar />
+    </main>
+  )
+}
+
+export default App
